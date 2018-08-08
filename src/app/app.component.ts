b76cf8010cda1e8e417e89012e984966108772a4
eric <eric.hu2514@gmail.com>
2018-08-08 18:23:42 +0800

    add button and input component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public isDisabledButton:boolean =false;
   title = 'app';
+  public formModule:FormGroup;
+  constructor(private fb:FormBuilder){
+    this.formModule = this.fb.group({
+      text:['12312',[]],
+    })
+  }
+
+  public clickHandle(){
+    console.log('click event');
+   /* this.isDisabledButton = true*/
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ButtonModule} from './button/button.module';
+import {EmModule} from './em.module';
+import {InputsModule} from './inputs/inputs.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ButtonModule,
+    EmModule,
+    InputsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
